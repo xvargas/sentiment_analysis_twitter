5041f4c33b89c5e1ee845b8eaa0796fbc8f17272
xvargas <ximenavargasu@gmail.com>
2021-01-09 18:39:19 -0500

    Update twitter_credentials.py

--- twitter_credentials.py
+++ twitter_credentials.py
@@ -1,5 +1,5 @@
 # Variables that contains the user credentials to access Twitter API
-ACCESS_TOKEN = "1664032136-7VYSsEsXI3CdXaDrLP4sMnbv8R0hcE3frs0glWg"
-ACCESS_TOKEN_SECRET = "bNFeTgIPWXMc8HbFpHN6pUCDhxNLXduEQJQgb3hUXB8m4"
-CONSUMER_KEY = "u9fTQbm9d59SvIb3dNgDtef9d"
-CONSUMER_SECRET = "waJeI53uscrkYl8WwOqwZovzXHnS8XbYKv1ohxWgg7MrO9oJJr"
+ACCESS_TOKEN = "xxxxx"
+ACCESS_TOKEN_SECRET = "xxxxx"
+CONSUMER_KEY = "xxxxx"
+CONSUMER_SECRET = "xxxxx"
